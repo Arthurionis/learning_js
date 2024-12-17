b6d0df1f1527c85c50eda2c2827b5eee8dd832df
Arthurionis <grimli158@gmail.com>
2024-12-17 18:02:06 +0300

    less9for

---
+++ less9for/main.js
@@ -0,0 +1,17 @@
+const scores = [ 78, 68, 45, 61, 46, 49, 63, 50, 55, 45, 69, 59, 80, 69, 77, 66, 46, 50, 53,
+     79, 55, 57, 65, 49, 42, 54, 47, 55, 40, 44, 43, 68, 52, 79, 56, 33]
+
+/*let i=0
+let output
+while(i<scores.length) {
+    output = "bubble solutins # " + i + "scores: " +scores[i]
+    console.log(output)
+    i++
+}*/
+
+for (let i=0; i<scores.length; i++) {
+    output = "bubble solutins # " + i + "scores: " +scores[i]
+    console.log(output)
+}
+
+console.log("All tets: "+scores.length)
