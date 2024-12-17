6a3e7cb728b7e542829944ee190f19679539b28f
Arthurionis <grimli158@gmail.com>
2024-12-17 18:45:20 +0300

    refactoringLes9

--- less9for/main.js
+++ less9for/main.js
@@ -1,32 +1,31 @@
-const scores = [ 78, 68, 45, 61, 46, 49, 63, 50, 55, 45, 69, 59, 80, 69, 77, 66, 46, 50, 53,
-     79, 55, 57, 65, 49, 42, 54, 47, 80, 40, 44, 43, 68, 52, 79, 56, 33]
+const scores = [78, 68, 45, 61, 46, 49, 63, 50, 55, 45, 69, 59, 80, 69, 77, 66, 46, 50, 53,
+    79, 55, 57, 65, 49, 42, 54, 47, 80, 40, 44, 43, 68, 52, 79, 56, 33]
 
-/*let i=0
-let output
-while(i<scores.length) {
-    output = "bubble solutins # " + i + "scores: " +scores[i]
-    console.log(output)
-    i++
-}*/
-
-let highScore=0;
-
-for (let i=0; i<scores.length; i++) {
-    output = "bubble solutins # " + i + "scores: " +scores[i]
-    console.log(output)
-    if (highScore < scores[i]){
-        highScore = scores[i]
-    }
+function getAndPrintHighScores(array) {
+    let output;
+    let highScore = 0;
+    for (let i = 0; i < array.length; i++) {
+        output = "bubble solutins # " + i + "scores: " + array[i]
+        console.log(output)
+        if (highScore < scores[i]) {
+            highScore = scores[i]
+        }
+    } return highScore
 }
 
-console.log("All tets: "+scores.length)
-console.log(" score: "+highScore)
+function getNumbersBestScores(ara, value) {
+    const bestScores = []
+    for (let i = 0; i < ara.length; i++) {
 
-const bestScores=[]
-for (let i=0; i<scores.length; i++){
-
-if (highScore ===scores[i]){
-    bestScores.push(i)
+        if (value === ara[i]) {
+            bestScores.push(i)
+        }
     }
+    return bestScores
 }
-console.log("solutions with highest score:  "+bestScores);
+
+const highScore = getAndPrintHighScores(scores)
+const bestScores = getNumbersBestScores(scores, highScore)
+console.log("All tets: " + scores.length)
+console.log("Highest score: " + highScore)
+console.log("solutions with highest score:  " + bestScores);
