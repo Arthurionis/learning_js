1078e8d31f018fd6387b97f07c107bc4cb00f1cd
Arthurionis <grimli158@gmail.com>
2024-12-17 16:42:33 +0300

    addNewFunction

--- less8phrase_generator/main.js
+++ less8phrase_generator/main.js
@@ -1,18 +1,12 @@
-
+function getRandomElement(array){
+const RandomIndex=Math.floor(Math.random()*array.lenghth)
+return array[RandomIndex]
+}
 function makePhrases() {
-    const word1 = ["My cat", "My dog", "My parrot"];
-    const word2 = ["loves to eat", "wants to chew", "always looking"];
-    const word3 = ["carrot", "pasta", "bone"];
-
-    const randomIndex1 = Math.floor(Math.random() * 3);
-    const randomIndex2 = Math.floor(Math.random() * 3);
-    const randomIndex3 = Math.floor(Math.random() * 3);
-
-    const phrase = word1[randomIndex1] + " " + word2[randomIndex2] + " " + word3[randomIndex3];
-    
-    return phrase
+    const words1 = ["My cat", "My dog", "My parrot"];
+    const words2 = ["loves to eat", "wants to chew", "always looking"];
+    const words3 = ["carrot", "pasta", "bone"];
+        return getRandomElemen(words1)+" "+getRandomElemen(words)+" "+getRandomElemen(words3)
 }
 
-const result = makePhrases()
-alert(result)
-console.log(result)
+alert(makePhrases())
